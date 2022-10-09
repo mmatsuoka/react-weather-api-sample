6902e407469006f8defd2dad86f1bd6dcdfc3d3a
DESKTOP-60JA8VD\matsu <matsuoka.m@gmail.com>
2022-10-09 22:34:08 +0900

    都市名の入力を保持するStateを追加。

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,17 @@
 // Form.tsx
 
+import { useState } from "react";
+
 const Form = () => {
+
+    const [city, setCity] = useState<string>("");
+
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名"/>
+            <input type="text" name="city" placeholder="都市名"
+            onChange={e=> setCity(e.target.value)}
+            />
+            {city}
             <button type="submit">都市の天気予報を取得する。</button>
         </form>
 
