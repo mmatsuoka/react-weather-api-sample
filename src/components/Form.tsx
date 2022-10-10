4bd87a129b8273ee2b9f4150e633c129fee9be88
DESKTOP-60JA8VD\matsu <matsuoka.m@gmail.com>
2022-10-10 20:10:41 +0900

    https://www.weatherapi.com/ が提供しているAPIから天気予報を取得できることを確認。

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,17 @@ import { useState } from "react";
 const Form = () => {
 
     const [city, setCity] = useState<string>("");
+    
+    /**
+     * https://www.weatherapi.com/ が提供しているAPIから天気予報を取得する。
+     */
+    const getWeather = (e:any) => {
+
+        e.preventDefault();
+
+        fetch("https://api.weatherapi.com/v1/current.json?key=420a5465eb6f4f628e0141918220910&q=London&aqi=no")
+        .then(response => console.log(response.json()) )
+    }
 
     return (
         <form>
@@ -12,10 +23,11 @@ const Form = () => {
             onChange={e=> setCity(e.target.value)}
             />
             {city}
-            <button type="submit">都市の天気予報を取得する。</button>
+            <button type="submit" onClick={getWeather}>都市の天気予報を取得する。</button>
         </form>
 
     );
+
 }
 
 export default Form;
