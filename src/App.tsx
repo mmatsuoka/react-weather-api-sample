4fe2dc66c16335bfee258c66c2a7bd6d382d7e77
DESKTOP-60JA8VD\matsu <matsuoka.m@gmail.com>
2022-10-10 20:20:06 +0900

    取得結果を表示するResultsコンポーネントを追加。

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Title from './components/Title';
 import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
+import Result from './components/Results';
 
 function App() {
   return (
     <div className="test">
       <Title/>
       <Form/>
+      <Results/>
     </div>
   );
 }
